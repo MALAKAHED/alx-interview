e650967024ffc2556c4595739fc8ac8df8148e34
MALAKAHED <lokamalek776@gmail.com>
2024-12-03 18:42:09 +0200

    This is readme file

---
+++ 0x09-island_perimeter/README.d
@@ -0,0 +1,14 @@
+# Island Perimeter
+
+## Description
+
+This project is about calculating the perimeter of an island represented by a grid. The grid is a list of lists of integers where:
+- 0 represents water
+- 1 represents land
+
+The grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+## Usage
+
+To use the function, you need to call the `island_perimeter` function with the grid as an argument.
+
